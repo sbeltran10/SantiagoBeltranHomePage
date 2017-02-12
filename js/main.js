4ebd44ad73fa823f06214aabd990a15669e18043
Santiago Beltran Caicedo <Santiago Beltran Caicedo>
2017-02-11 17:06:48 -0800

    Cambios varios, modificados botonos, iconos de redes sociales

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var main = function () {
+    $("#interes-prof").on("click",function () {
+        $("#shown-content").slideToggle(400);
+        console.log("what");
+    })
+};
+$(document).ready(main);
+
+
+
