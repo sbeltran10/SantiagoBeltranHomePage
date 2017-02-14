25601ea6b29a05d3bc41871526f365c36ef0fb28
Santiago Beltran Caicedo <Santiago Beltran Caicedo>
2017-02-13 23:16:35 -0800

    Mas cambios, nuevo estilo, incorporado un template para la parte de intereses

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 var main = function () {
-    $("#interes-prof").on("click",function () {
+    $("#interes-prof").on("click", function () {
+        $("#shown-content").slideUp(400);
+        $("#content-experiencia").slideUp(400);
+        $("#content-intereses").slideToggle(400);
+    });
+
+    $("#experiencia-habilidades").on("click", function () {
+        $("#shown-content").slideUp(400);
+        $("#content-intereses").slideUp(400);
+        $("#content-experiencia").slideToggle(400);
+    });
+
+    $("#hobbies").on("click", function () {
+        $("#content-intereses").slideUp(400);
+        $("#content-experiencia").slideUp(400);
         $("#shown-content").slideToggle(400);
-        console.log("what");
-    })
+    });
 };
 $(document).ready(main);
 
