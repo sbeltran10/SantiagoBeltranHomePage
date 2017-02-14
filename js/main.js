c0fb7df0ea7347cd2f9ea8f6655a9d58f6af1557
sbeltran10 <s.beltran10@uniandes.edu.co>
2017-02-14 14:01:31 -0500

    Primeros avances para leer los JSon

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 var main = function () {
+
+    //Asigna funciones a los botones principales
     $("#interes-prof").on("click", function () {
         $("#shown-content").slideUp(400);
         $("#content-experiencia").slideUp(400);
@@ -16,6 +18,21 @@ var main = function () {
         $("#content-experiencia").slideUp(400);
         $("#shown-content").slideToggle(400);
     });
+
+    //Se encarga de llenar el contenido de la sección de experiencia y habilidades
+    $.getJSON("test.json", function (json) {
+        var trabajos = JSON.parse(json);
+        console.log(trabajos);
+    });
+
+    $.getJSON("test.json", function (json) {
+        var proyectos = JSON.parse(json);
+    });
+
+    $.getJSON("test.json", function (json) {
+        var habilidades = JSON.parse(json);
+    });
+
 };
 $(document).ready(main);
 
